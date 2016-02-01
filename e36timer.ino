353b845e4279d52111b40e4a63224c17efb2ce4b
PeterWithers <peter-gthb@bambooradical.com>
2016-02-01 20:05:02 +0100

    Update e36timer.ino

--- e36timer.ino
+++ e36timer.ino
@@ -13,5 +13,11 @@ int indicatorLed = PB1;
 void setup() {
   	pinMode(led, OUTPUT);
   	// todo: set up the PWM timer with a frequency to suit the servo and ESC, probably a 20ms period and pulse width of 1 to 2 ms.
+    // PWM Frequency: 50 kHz 
+    // Clock Selection: PCK/8
+    // CS1[3:0]: 0100
+    // OCR1C: 159
+    // RESOLUTION: 7.3
+    
   	// todo: keep in mind that timer0 is used for functions like delay() so timer1 is a better choice.
 }
