52b7b0c05174f1e609bb14a5a0d91016de019895
Peter Withers <peter@bambooradical.com>
2016-02-13 19:48:37 +0100

    Migrated code into the C project.

--- E36Timer/E36Timer/main.c
+++ E36Timer/E36Timer/main.c
@@ -1,16 +1,33 @@
 /*
+ * Copyright (C) 2016 Peter Withers
+ */
+
+/*
  * E36Timer.c
  *
  * Created: 13/02/2016 18:00:32
- * Author : Peter Withers
+ * Author : Peter Withers <peter@gthb-bambooradical.com>
  */ 
 
 #include <avr/io.h>
 
+#define IndicatorLed PB1
+#define ServoPWM	 PB0
+#define EscPWM		 PB4
+#define ButtonPin	 PB2
+
+#define MaxOCR0A 125
+#define MinOCR0A 60
+
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRB |= 1 << IndicatorLed; // set the LED to output
+	DDRB |= 1 << ServoPWM; // set the servo to output
+	DDRB |= 1 << EscPWM; // set the ESC to output
+	DDRB &= ~(1 << ButtonPin); // set the button to input
+	PORTB |= 1 << ButtonPin; // activate the internal pull up resistor
+
     while (1) 
     {
     }
