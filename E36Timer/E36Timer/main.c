5d7e93e18d5cff38f7abbba4c17352ca6b8a9bd7
Peter Withers <peter@bambooradical.com>
2016-02-13 19:03:05 +0100

    Started to working in atmelstudio and migrating to C.

---
+++ E36Timer/E36Timer/main.c
@@ -0,0 +1,18 @@
+/*
+ * E36Timer.c
+ *
+ * Created: 13/02/2016 18:00:32
+ * Author : Peter Withers
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
