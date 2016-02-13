a20f5a20b061ae70c14afcab270b651f776e101c
Peter Withers <peter@bambooradical.com>
2016-02-13 20:26:56 +0100

    Migrated code into the C project.

--- E36Timer/E36Timer/main.c
+++ E36Timer/E36Timer/main.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (C) 2016 Peter Withers
- */
+* Copyright (C) 2016 Peter Withers
+*/
 
 /*
- * E36Timer.c
- *
- * Created: 13/02/2016 18:00:32
- * Author : Peter Withers <peter@gthb-bambooradical.com>
- */ 
+* E36Timer.c
+*
+* Created: 13/02/2016 18:00:32
+* Author : Peter Withers <peter@gthb-bambooradical.com>
+*/
 
 #include <avr/io.h>
 
@@ -19,17 +19,78 @@
 #define MaxOCR0A 125
 #define MinOCR0A 60
 
+#define ButtonIsDown 0 // todo: test the button state // digitalRead(buttonPin) == HIGH
 
-int main(void)
-{
+#define TurnOnLed ; // todo digitalWrite(indicatorLed, HIGH);
+#define TurnOffLed ; // todo digitalWrite(indicatorLed, LOW);
+
+void loadSavedSettings() {
+	//EEPROM.write(a,b);
+	//z = EEPROM.read(a);
+}
+
+void setupRegisters() {
 	DDRB |= 1 << IndicatorLed; // set the LED to output
 	DDRB |= 1 << ServoPWM; // set the servo to output
 	DDRB |= 1 << EscPWM; // set the ESC to output
 	DDRB &= ~(1 << ButtonPin); // set the button to input
 	PORTB |= 1 << ButtonPin; // activate the internal pull up resistor
+}
 
-    while (1) 
-    {
-    }
+int getInput() {
+	int hitCount = 0;
+	for (int counter = 10; counter > 0; counter--) {
+		if (ButtonIsDown) {
+			hitCount++;
+			TurnOnLed;
+			} else {
+			TurnOffLed;
+		}
+		//delay(500);
+	}
+	TurnOffLed;
+	if (hitCount > 5) {
+		return -1;
+		} else if (hitCount > 2) {
+		return 1;
+		} else {
+		return 0;
+	}
+}
+
+void showMotorTime() {
+	int inputTimeout = 10;
+	while (inputTimeout > 0) {
+		int input = getInput();
+		if (input != 0) {
+			OCR0A += input;
+			} else {
+			inputTimeout--;
+		}
+	}
+	//delay(1000);
+}
+
+void showDtTime() {
+	OCR0A = 80;
+	//delay(1000);
+}
+
+void startFlightMode() {
+	OCR0A = 60;
+}
+
+int main(void)
+{
+	loadSavedSettings();
+	setupRegisters();
+	loadSavedSettings();
+	showMotorTime();
+	showDtTime();
+	startFlightMode();
+
+	while (1)
+	{
+	}
 }
 
