0d84e6f36b237b6ccaef3aa251c6e7987f1eea78
Peter Withers <peter@bambooradical.com>
2016-02-19 23:49:33 +0100

    Updated the build script after the refactoring.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Author : Peter Withers <peter@gthb-bambooradical.com>
 
 rm main.o main.elf
-avr-gcc -Wall -Os -DF_CPU=8000000 -mmcu=attiny85 -c E36Timer/E36Timer/main.c -o main.o
+avr-gcc -Wall -Os -DF_CPU=8000000 -mmcu=attiny85 -c main.c -o main.o
 avr-gcc -Wall -Os -DF_CPU=8000000 -mmcu=attiny85 -o main.elf main.o
 avr-objcopy -j .text -j .data -O ihex main.elf main.hex
 avr-size --format=avr --mcu=attiny85 main.elf
